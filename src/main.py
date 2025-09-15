b1761bc5c68f28c5e7d2ab45d653b1030b79e30b
DaviidSantos <89614359+DaviidSantos@users.noreply.github.com>
2025-09-15 19:50:02 -0300

    shortening the commit hash

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ def details():
     return jsonify({
         'time': datetime.datetime.now().strftime("%I:%M:%p on %B %d, %Y"),
         'hostname': socket.gethostname(),
-        'message': 'You are doing great human! Keep at it!'
+        'message': 'You are doing great human! Keep at it! 🔥'
     }), 200
 
 @app.route('/api/v1/healthz')
